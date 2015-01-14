e2ef981db3b4bb9780f56f3ad26e17f420ab0533
mosson <cucation@gmail.com>
2015-01-14 14:59:32 +0900

    テスト記述をリファクタリング
    described_classを使うと楽になるため
    可読性をあげるため

--- spec/lib/fizzbuzz01_spec.rb
+++ spec/lib/fizzbuzz01_spec.rb
@@ -2,23 +2,26 @@ require "spec_helper"
 require "fizzbuzz01"
 
 describe Fizzbuzz01 do
-  context '引数が不正な場合' do
-    it '引数がなければArgumentErrorをあげる' do
-      expect { Fizzbuzz01.calc }.to raise_error ArgumentError
-    end
+  describe 'クラスメソッド' do
+    describe '#calc' do
+      context '引数が不正な場合' do
+        it '引数がなければArgumentErrorをあげる' do
+          expect { described_class.calc }.to raise_error ArgumentError
+        end
 
-    it '引数が整数でなければTypeErrorをあげる' do
-      expect { Fizzbuzz01.calc 'a', 1 }.to raise_error TypeError
-      expect { Fizzbuzz01.calc 1, 'a' }.to raise_error TypeError
-    end
-  end
+        it '引数が整数でなければTypeErrorをあげる' do
+          expect { described_class.calc 'a', 1 }.to raise_error TypeError
+          expect { described_class.calc 1, 'a' }.to raise_error TypeError
+        end
+      end
 
-  # 標準出力のテスト方法: https://www.relishapp.com/rspec/rspec-expectations/docs/built-in-matchers/output-matcher
-  context '引数が正しい場合' do
-    it 'nからmまで数字を増やしていき3で割り切れるときFizz、5で割り切れるときBuzz, 15で割り切れるときFizzBuzzを出力する' do
-      expected = ['FizzBuzz', 1, 2, 'Fizz', 4, 'Buzz', 'Fizz', 7, 8, 'Fizz', "Buzz\n"].join("\n")
-      expect { Fizzbuzz01.calc(0,10) }.to output(expected).to_stdout
+      # 標準出力のテスト方法: https://www.relishapp.com/rspec/rspec-expectations/docs/built-in-matchers/output-matcher
+      context '引数が正しい場合' do
+        it 'nからmまで数字を増やしていき3で割り切れるときFizz、5で割り切れるときBuzz, 15で割り切れるときFizzBuzzを出力する' do
+          expected = ['FizzBuzz', 1, 2, 'Fizz', 4, 'Buzz', 'Fizz', 7, 8, 'Fizz', "Buzz\n"].join("\n")
+          expect { described_class.calc(0,10) }.to output(expected).to_stdout
+        end
+      end
     end
   end
 end
-
