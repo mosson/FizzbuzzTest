326dd74ce287bc31d41decd22da6451280b95767
mosson <cucation@gmail.com>
2015-01-14 15:34:19 +0900

    さらにリファクタリングを進めた
    コードのテストカバレッジを高めるためにスプラウト
    モジュールに変更

--- lib/fizzbuzz01.rb
+++ lib/fizzbuzz01.rb
@@ -1,22 +1,35 @@
-class Fizzbuzz01 # ファイル名とクラス名は一致させてください
-  class << self
-    # 副作用のないものはクラスメソッドにしたほうが良いと思います
-    # 入力値があったほうが良いと思います
+module Fizzbuzz01
+
+  module ModuleMethods
     def calc(n,m)
       fail TypeError unless n.is_a? Integer
       fail TypeError unless m.is_a? Integer
+      n.upto(m).each { |num| distribute num }
+    end
 
-      n.upto(m).each do |num|
-        if num % 15 == 0
-          puts "FizzBuzz"
-        elsif num % 5 == 0
-          puts "Buzz"
-        elsif num % 3 == 0
-          puts "Fizz"
-        else
-          puts num
-        end
-      end
+    def distribute(num)
+      return puts_fizz_buzz if num % 15 == 0
+      return puts_buzz if num % 5 == 0
+      return puts_fizz if num % 3 == 0
+      puts_num num
+    end
+
+    def puts_fizz_buzz
+      puts 'FizzBuzz'
+    end
+
+    def puts_buzz
+      puts 'Buzz'
+    end
+
+    def puts_fizz
+      puts "Fizz"
+    end
+
+    def puts_num(num)
+      puts num
     end
   end
+
+  extend ModuleMethods
 end
--- spec/lib/fizzbuzz01_spec.rb
+++ spec/lib/fizzbuzz01_spec.rb
@@ -2,7 +2,7 @@ require "spec_helper"
 require "fizzbuzz01"
 
 describe Fizzbuzz01 do
-  describe 'クラスメソッド' do
+  describe 'モジュールメソッド' do
     describe '#calc' do
       context '引数が不正な場合' do
         it '引数がなければArgumentErrorをあげる' do
@@ -10,17 +10,102 @@ describe Fizzbuzz01 do
         end
 
         it '引数が整数でなければTypeErrorをあげる' do
-          expect { described_class.calc 'a', 1 }.to raise_error TypeError
-          expect { described_class.calc 1, 'a' }.to raise_error TypeError
+          expect { described_class.calc('a', 1) }.to raise_error TypeError
+          expect { described_class.calc(1, 'a') }.to raise_error TypeError
         end
       end
 
-      # 標準出力のテスト方法: https://www.relishapp.com/rspec/rspec-expectations/docs/built-in-matchers/output-matcher
       context '引数が正しい場合' do
-        it 'nからmまで数字を増やしていき3で割り切れるときFizz、5で割り切れるときBuzz, 15で割り切れるときFizzBuzzを出力する' do
-          expected = ['FizzBuzz', 1, 2, 'Fizz', 4, 'Buzz', 'Fizz', 7, 8, 'Fizz', "Buzz\n"].join("\n")
-          expect { described_class.calc(0,10) }.to output(expected).to_stdout
+        it 'nからmまでの数字を#distributeに渡す' do
+          expect(described_class).to receive(:distribute).with(0)
+          expect(described_class).to receive(:distribute).with(1)
+          expect(described_class).to receive(:distribute).with(2)
+          expect(described_class).to receive(:distribute).with(3)
+          expect(described_class).not_to receive(:distribute).with(3)
+
+          described_class.calc(0, 3)
         end
+      end
+    end
+
+    describe '#distribute(num)' do
+      context '引数が正しい場合' do
+        context '3で割り切れるとき' do
+          it 'puts_fizzを呼ぶ' do
+            expect(described_class).not_to receive(:puts_fizz_buzz)
+            expect(described_class).not_to receive(:puts_buzz)
+            expect(described_class).not_to receive(:puts_num)
+            expect(described_class).to receive(:puts_fizz)
+            described_class.distribute(3)
+          end
+        end
+
+        context '5で割り切れるとき' do
+          it 'puts_buzzを呼ぶ' do
+            expect(described_class).not_to receive(:puts_fizz_buzz)
+            expect(described_class).not_to receive(:puts_fizz)
+            expect(described_class).not_to receive(:puts_num)
+            expect(described_class).to receive(:puts_buzz)
+            described_class.distribute(5)
+          end
+        end
+
+        context '15で割り切れるとき' do
+          it '#puts_fizz_buzzを呼ぶ' do
+            expect(described_class).not_to receive(:puts_fizz)
+            expect(described_class).not_to receive(:puts_num)
+            expect(described_class).not_to receive(:puts_buzz)
+            expect(described_class).to receive(:puts_fizz_buzz)
+            described_class.distribute(15)
+          end
+        end
+
+        context 'いずれでもない場合' do
+          it '#puts_numを呼ぶ' do
+            expect(described_class).not_to receive(:puts_fizz)
+            expect(described_class).not_to receive(:puts_buzz)
+            expect(described_class).not_to receive(:puts_fizz_buzz)
+            expect(described_class).to receive(:puts_num).with(2)
+            described_class.distribute(2)
+          end
+        end
+      end
+
+      context '引数が正しくない場合' do
+        it '引数が渡されない場合はTypeErrorをあげる' do
+          expect { described_class.distribute }.to raise_error ArgumentError
+        end
+      end
+    end
+
+    describe '#puts_fizz_buzz' do
+      it '標準出力にFizzBuzzを出力する' do
+        expect { described_class.puts_fizz_buzz }.to output("FizzBuzz\n").to_stdout
+      end
+    end
+
+    describe '#puts_buzz' do
+      it '標準出力にBuzzを出力する' do
+        expect { described_class.puts_buzz }.to output("Buzz\n").to_stdout
+      end
+    end
+
+    describe '#puts_fizz' do
+      it '標準出力にFizzを出力する' do
+        expect { described_class.puts_fizz }.to output("Fizz\n").to_stdout
+      end
+    end
+
+    describe '#puts_puts_num(num)'
+    context '引数が正しい場合' do
+      it '引数がない場合ArgumentErrorをあげる' do
+        expect { described_class.puts_num }.to raise_error ArgumentError
+      end
+    end
+
+    context '引数が正しくない場合' do
+      it '標準出力に引数を出力する' do
+        expect { described_class.puts_num(3) }.to output("3\n").to_stdout
       end
     end
   end
